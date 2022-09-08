21e3a2db7c85a8167525569375111988c5f7d91a
Lobachev Evgeniy <e.lobachev@bk.ru>
2022-09-08 15:22:40 +0400

    task1

--- task1/task_1.cs
+++ task1/task_1.cs
@@ -1,6 +1,10 @@
-﻿Console.Clear();
-
-Console.Write("Введи трёхзначное число: ");
-int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
-string stringNumber = Convert.ToString(threeDigitNumber);
-Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите трёхзначное число: ");
+        int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+        string stringNumber = Convert.ToString(threeDigitNumber);
+        Console.WriteLine("Вторая цифра этого числа -> " + stringNumber[1]);
+    }
+}
