2eab738c7cdfc82fd7062c4951e2656db7f9194a
Lobachev Evgeniy <e.lobachev@bk.ru>
2022-09-08 15:28:52 +0400

    task2

---
+++ task2/task_2.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите число: ");
+        int anyNumber = Convert.ToInt32(Console.ReadLine());
+        string anyNumberText = Convert.ToString(anyNumber);
+        if (anyNumberText.Length > 2)
+        {
+            Console.WriteLine("Третья цифра -> " + anyNumberText[2]);
+        }
+        else
+        {
+            Console.WriteLine("-> Третьей цифры нет");
+        }
+    }
+}
