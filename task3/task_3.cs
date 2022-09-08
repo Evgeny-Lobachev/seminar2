cd0af747b5e58b288b80650429fc054cd5e6889a
Lobachev Evgeniy <e.lobachev@bk.ru>
2022-09-08 15:36:22 +0400

    task3

--- task3/task_3.cs
+++ task3/task_3.cs
@@ -1,1 +1,23 @@
-﻿
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите цифру, обозначающую день недели: ");
+        int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+        void CheckingTheDayOfTheWeek(int dayNumber)
+        {
+            if (dayNumber == 6 || dayNumber == 7)
+            {
+                Console.WriteLine("Этот день - выходной");
+            }
+            else if (dayNumber < 1 || dayNumber > 7)
+            {
+                Console.WriteLine("Это число не день недели");
+            }
+            else Console.WriteLine("Этот день не выходной");
+        }
+
+        CheckingTheDayOfTheWeek(dayNumber);
+    }
+}
